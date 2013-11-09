edce74dcfd46d16ec9c1b08eb5327cb8aababc52
Ryo Nakamura <r7kamura@gmail.com>
2013-11-10 04:57:14 +0900

    Basic design

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 require "slim"
 
-set :blog_title, "r7kamura"
-set :build_dir, "blog"
+set :blog_title, "r7kamura blog"
+set :blog_description, "marking down about tech and journal."
 set :layout_engine, :slim
 
 page "/feed.xml", layout: false
@@ -9,19 +9,19 @@ page "/feed.xml", layout: false
 Time.zone = "Tokyo"
 
 activate :blog do |blog|
-  blog.calendar_template = "calendar.html"
-  blog.day_link = ":year/:month/:day"
-  blog.month_link = ":year/:month"
-  blog.permalink = ":year/:month/:day/:title"
-  blog.sources = "articles/:year-:month-:day-:title.html"
-  blog.tag_template = "tag.html"
-  blog.taglink = "tags/:tag"
-  blog.year_link = ":year"
-  blog.paginate = false
+  # blog.day_link = ":year/:month/:day"
   # blog.layout = "layout"
+  # blog.month_link = ":year/:month"
   # blog.page_link = "page/:num"
   # blog.per_page = 10
   # blog.prefix = "blog"
   # blog.summary_length = 250
   # blog.summary_separator = /(READMORE)/
+  # blog.year_link = ":year"
+  blog.calendar_template = "calendar.html"
+  blog.paginate = false
+  blog.permalink = ":year/:month/:day/:title"
+  blog.sources = "articles/:year-:month-:day-:title.html"
+  blog.tag_template = "tag.html"
+  blog.taglink = "tags/:tag"
 end
--- source/articles/2013-11-10-hello-world.md
+++ source/articles/2013-11-10-hello-world.md
@@ -4,5 +4,13 @@ date: 2013-11-10
 tags: diary
 ---
 
-This is an example article.
-You probably want to delete it and write your own articles!
+書く場所の無い微妙に技術的な知識が増えてきたので、
+[r7kamura.hatenablog.com](http://r7kamura.hatenablog.com) とは別に新しくブログを作った。
+住み分け方は特に考えていないけれど、多分このブログは飽きてその内更新しなくなると思う。
+ブログの選定にあたっては、[tumblr](http://tumblr.com)、
+[medium](https://medium.com/)、
+[scriptogr.am](http://scriptogr.am)を考えた後、
+今回は[Middleman](http://middlemanapp.com/)を利用することにした。
+仕組み的には、DropboxとGitHubとTravis-CIを使っている。
+編集後にGitHubにPushするのが若干面倒なので、
+代わりにVPSで動かしているプログラムがDropboxを監視してGitHubにpushしている。
