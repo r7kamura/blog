30761908430b7872d8458e0121aaec3da4b6437a
Ryo Nakamura <r7kamura@gmail.com>
2013-11-10 01:09:06 +0900

    Generate blog template

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
-source "https://rubygems.org"
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'http://rubygems.org'
 
-gem "middleman", "3.2.0"
-gem "middleman-blog", "3.4.1"
-gem "middleman-livereload", "3.1.0"
-gem "slim", "2.0.2"
+gem "middleman", "~> 3.2.0"
+gem "middleman-blog", "~> 3.4.1"
+
+# For feed.xml.builder
+gem "builder", "~> 3.0"
--- config.rb
+++ config.rb
@@ -1,9 +1,26 @@
-require "slim"
+Time.zone = "Tokyo"
+
+activate :blog do |blog|
+  blog.calendar_template = "calendar.html"
+  blog.tag_template = "tag.html"
+  # blog.day_link = ":year/:month/:day.html"
+  # blog.default_extension = ".markdown"
+  # blog.layout = "layout"
+  # blog.month_link = ":year/:month.html"
+  # blog.page_link = "page/:num"
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.permalink = ":year/:month/:day/:title.html"
+  # blog.prefix = "blog"
+  # blog.sources = ":year-:month-:day-:title.html"
+  # blog.summary_length = 250
+  # blog.summary_separator = /(READMORE)/
+  # blog.taglink = "tags/:tag.html"
+  # blog.year_link = ":year.html"
+end
+
+page "/feed.xml", :layout => false
 
 set :css_dir, "stylesheets"
-set :js_dir, "javascripts"
 set :images_dir, "images"
-
-configure :build do
-  activate :blog
-end
+set :js_dir, "javascripts"
