3eea761d42fb44286787b6cbf8d6057582df3293
Ryo Nakamura <r7kamura@gmail.com>
2013-11-10 02:23:54 +0900

    Slimnize

--- config.rb
+++ config.rb
@@ -1,26 +1,27 @@
+require "slim"
+
+set :blog_title, "r7kamura"
+set :build_dir, "blog"
+set :layout_engine, :slim
+
+page "/feed.xml", layout: false
+
 Time.zone = "Tokyo"
 
 activate :blog do |blog|
   blog.calendar_template = "calendar.html"
+  blog.day_link = ":year/:month/:day"
+  blog.month_link = ":year/:month"
+  blog.permalink = ":year/:month/:day/:title"
+  blog.sources = "articles/:year-:month-:day-:title.html"
   blog.tag_template = "tag.html"
-  # blog.day_link = ":year/:month/:day.html"
-  # blog.default_extension = ".markdown"
+  blog.taglink = "tags/:tag"
+  blog.year_link = ":year"
+  blog.paginate = false
   # blog.layout = "layout"
-  # blog.month_link = ":year/:month.html"
   # blog.page_link = "page/:num"
-  # blog.paginate = true
   # blog.per_page = 10
-  # blog.permalink = ":year/:month/:day/:title.html"
   # blog.prefix = "blog"
-  # blog.sources = ":year-:month-:day-:title.html"
   # blog.summary_length = 250
   # blog.summary_separator = /(READMORE)/
-  # blog.taglink = "tags/:tag.html"
-  # blog.year_link = ":year.html"
 end
-
-page "/feed.xml", :layout => false
-
-set :css_dir, "stylesheets"
-set :images_dir, "images"
-set :js_dir, "javascripts"
