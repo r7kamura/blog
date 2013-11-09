89d6500de90b4973d970b504f86592641f6aa1b2
Ryo Nakamura <r7kamura@gmail.com>
2013-11-10 07:43:15 +0900

    Remove unnecessary code comment

--- config.rb
+++ config.rb
@@ -9,15 +9,6 @@ page "/feed.xml", layout: false
 Time.zone = "Tokyo"
 
 activate :blog do |blog|
-  # blog.day_link = ":year/:month/:day"
-  # blog.layout = "layout"
-  # blog.month_link = ":year/:month"
-  # blog.page_link = "page/:num"
-  # blog.per_page = 10
-  # blog.prefix = "blog"
-  # blog.summary_length = 250
-  # blog.summary_separator = /(READMORE)/
-  # blog.year_link = ":year"
   blog.calendar_template = "calendar.html"
   blog.paginate = false
   blog.sources = "articles/:year-:month-:day-:title.html"
