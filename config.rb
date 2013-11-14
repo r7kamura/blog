77ac44b6eb71c79cf826177bed09490bebeddffe
Ryo Nakamura <ryo-nakamura@cookpad.com>
2013-11-14 18:31:26 +0900

    Refactor: article layout

--- config.rb
+++ config.rb
@@ -4,6 +4,7 @@ set :blog_title, "r7kamura blog"
 set :blog_description, "marking down about tech."
 set :layout_engine, :slim
 
+page "/articles/*.html", layout: :article_layout
 page "/feed.xml", layout: false
 
 Time.zone = "Tokyo"
