c389e2996ac1ad76ddbf433d42157756664f38f1
Ryo Nakamura <r7kamura@gmail.com>
2014-07-17 11:15:06 +0900

    Add an article: flynn-overview

---
+++ articles/2014-07-17-flynn-overview.md
@@ -0,0 +1,7 @@
+---
+title: Flynn Overview
+---
+
+![](/images/2014-07-17/flynn-overview.png)
+
+[Flynn](https://flynn.io/) という、コンテナを複数ホストで動作させるPaaS実装の全体像。
---
+++ dots/2014-07-17-flynn-overview.rb
@@ -0,0 +1,135 @@
+require "gviz"
+
+node_options = { shape: "box3d", colorscheme: "blues8", style: "filled" }
+
+Graph(:G, :digraph) do
+  nodes node_options
+
+  route :shell => :gitreceived
+  edge "shell_gitreceived", label: "git-push(1)"
+
+  route :gitreceived => :shelf
+  edge "gitreceived_shelf", label: "upload slug"
+
+  route :gitreceived => :controller
+  edge "gitreceived_controller", label: "notify deployment"
+
+  route :controller => [:hostA, :hostB]
+  edge "controller_hostA", label: "deploy"
+  edge "controller_hostB", label: "deploy"
+
+  route :userAgent => :strowger
+  edge "userAgent_strowger", label: "request", color: "maroon"
+
+  route :strowger => [:hostA, :hostB]
+  edge "strowger_hostA", label: "proxy", color: "maroon"
+  edge "strowger_hostB", label: "proxy", color: "maroon"
+
+  route(
+    :etcdS => :etcdG,
+    :etcdG => :etcdC,
+    :etcdC => :etcdB,
+    :etcdB => :etcdA,
+    :etcdA => :etcdR,
+  )
+  edge "etcdG_*", arrowhead: "none", style: "dotted"
+  edge "etcdS_*", arrowhead: "none", style: "dotted"
+  edge "etcdC_*", arrowhead: "none", style: "dotted"
+  edge "etcdB_*", arrowhead: "none", style: "dotted"
+  edge "etcdA_*", arrowhead: "none", style: "dotted"
+  edge "etcdR_*", arrowhead: "none", style: "dotted"
+
+  subgraph do
+    global label: "Cloud"
+
+    subgraph do
+      global label: "Cluster"
+
+      subgraph do
+        global label: "Host A"
+        nodes node_options
+        node :hostA, fillcolor: 3, label: "host"
+        node :discoverdA, label: "discoverd"
+        node :etcdA, label: "etcd"
+        node :containerA, label: "container", fillcolor: 4
+        route :hostA => :containerA
+        route :hostA => :discoverdA
+        route :discoverdA => :etcdA
+        edge "hostA_containerA", color: "maroon", label: "deploy & proxy"
+      end
+
+      subgraph do
+        global label: "Host B"
+        nodes node_options
+        node :hostB, fillcolor: 3, label: "host"
+        node :discoverdB, label: "discoverd"
+        node :etcdB, label: "etcd"
+        node :containerB, label: "container", fillcolor: 4
+        route :hostB => :containerB
+        route :hostB => :discoverdB
+        route :discoverdB => :etcdB
+        edge "hostB_containerB", color: "maroon", label: "deploy & proxy"
+      end
+    end
+
+    subgraph do
+      global label: "Git server"
+      nodes node_options
+      node :gitreceived, fillcolor: 3
+      node :discoverdG, label: "discoverd"
+      node :etcdG, label: "etcd"
+      route :gitreceived => :discoverdG
+      route :discoverdG => :etcdG
+    end
+
+    subgraph do
+      global label: "Shelf server"
+      nodes node_options
+      node :shelf, fillcolor: 3
+      node :discoverdS, label: "discoverd"
+      node :etcdS, label: "etcd"
+      route :shelf => :discoverdS
+      route :discoverdS => :etcdS
+    end
+
+    subgraph do
+      global label: "API server"
+      nodes node_options
+      node :controller, fillcolor: 3
+      node :discoverdC, label: "discoverd"
+      node :etcdC, label: "etcd"
+      route :controller => :discoverdC
+      route :discoverdC => :etcdC
+    end
+
+    subgraph do
+      global label: "Reverse proxy"
+      nodes node_options
+      node :strowger, fillcolor: 3
+      node :discoverdR, label: "discoverd"
+      node :etcdR, label: "etcd"
+      route :strowger => :discoverdR
+      route :discoverdR => :etcdR
+    end
+  end
+
+  subgraph do
+    global label: "Internet"
+
+    subgraph do
+      global label: "Developer"
+      nodes node_options
+      node :shell, fillcolor: 2, label: "shell"
+      node :shell
+    end
+
+    subgraph do
+      global label: "User"
+      nodes node_options
+      node :userAgent, fillcolor: 2, label: "user agent"
+      node :userAgent
+    end
+  end
+
+  save "images/2014-07-17/flynn-overview", :png
+end
